1b895afbd37916d068816dc2fbcf59c9aa889de3
shrutislaxetti <39291059+shrutislaxetti@users.noreply.github.com>
2021-01-07 15:47:19 +0530

    Create lambdaFunction1.py

---
+++ src/lambdaFunction1.py
@@ -0,0 +1,10 @@
+import json
+   
+
+def helloFunction(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello Suraj!!!!!!!!!!!')
+    }
+   
